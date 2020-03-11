9dc2d3ca15dc062320650e02d5d70c3a0d7d1f30
e-dorigatti <emilio.dorigatti@gmail.com>
2020-03-11 10:49:27 +0100

    trying and failing to make pareto faster

--- pareto.py
+++ pareto.py
@@ -1,5 +1,6 @@
 import csv
 import logging
+import multiprocessing as mp
 
 import click
 import pyomo.environ as aml
@@ -20,6 +21,8 @@ from spacers.pcm import DoennesKohlbacherPcm
 @click.option('--mc-draws', '-n', default=100, help='How many Monte Carlo experiments to use')
 @click.option('--num-epitopes', '-e', default=5, help='How many epitopes in the vaccine')
 @click.option('--upper-bound', '-u', type=float, help='Upper bound for the effective immunogenicity')
+@click.option('--solution-epitopes', '-se', multiple=True, help='Initial epitopes for the warm start')
+@click.option('--solution-spacers', '-ss', multiple=True, help='Initial spacers for the warm start')
 @click.option('--log-file', type=click.Path(), help='Where to save the logs')
 @click.option('--verbose', is_flag=True, help='Print debug messages')
 @click.pass_context
@@ -30,11 +33,11 @@ def main(ctx=None, **kwargs):
     '''
     global LOGGER
     LOGGER = utilities.init_logging(kwargs.get('verbose', False), kwargs.get('log_file', None))
-    utilities.main_dispatcher(seqdesign_cli, LOGGER, ctx, kwargs)
+    utilities.main_dispatcher(pareto_cli, LOGGER, ctx, kwargs)
 
 
-def seqdesign_cli(input_epitopes, output_vaccine, rounds, increment, upper_bound,
-                  cleavage_prior, num_epitopes, mc_draws, **kwargs):
+def pareto_cli(input_epitopes, output_vaccine, rounds, increment, upper_bound, solution_epitopes,
+               solution_spacers, cleavage_prior, num_epitopes, mc_draws, **kwargs):
     # discard epitopes containing invalid amino acids
     epitope_data = utilities.load_epitopes(input_epitopes, None, None, None)
     pcm = DoennesKohlbacherPcm()
@@ -43,52 +46,61 @@ def seqdesign_cli(input_epitopes, output_vaccine, rounds, increment, upper_bound
     protein_coverage = utilities.compute_protein_coverage(epitope_data.values())
     LOGGER.info(f'Loaded {len(epitope_data)} epitopes')
 
-    constraints = [
-        spco.MinimumNTerminusCleavage(1.5),
-        spco.MinimumCTerminusCleavage(1.5),
-        spco.MaximumCleavageInsideEpitopes(0.0, 3),
-        spco.MinimumCoverageAverageConservation(protein_coverage, min_coverage=2, name='proteins'),
-    ]
-
     epitopes = list(epitope_data.keys())
     immunogens = [epitope_data[e]['immunogen'] for e in epitopes]
 
-    params = ModelParams(
-        all_epitopes=epitopes,
-        epitope_immunogen=immunogens,
-        min_spacer_length=4,
-        max_spacer_length=4,
-        vaccine_length=num_epitopes,
-    )
-
     problem = StrobeSpacer(
-        params=params,
-        vaccine_constraints=constraints,
+        params=ModelParams(
+            all_epitopes=epitopes,
+            epitope_immunogen=immunogens,
+            min_spacer_length=4,
+            max_spacer_length=4,
+            vaccine_length=num_epitopes,
+        ),
+        vaccine_constraints=[
+            spco.MinimumNTerminusCleavage(1.5),
+            spco.MinimumCTerminusCleavage(1.5),
+            spco.MaximumCleavageInsideEpitopes(0.5, 3),
+            spco.MonteCarloRecoveryEstimation(mc_draws, cleavage_prior),
+        ],
         vaccine_objective=spob.SimpleImmunogenicityObjective(),
         solver_type='gurobi_persistent',
     ).build_model()
 
     LOGGER.info('Optimizing simple immunogenicity first...')
-    _ = problem.solve()
+    problem.solve()
 
-    problem.add_constraint(spco.MonteCarloRecoveryEstimation(mc_draws, cleavage_prior))
+    LOGGER.info('Now optimizing effective immunogenicity...')
     problem.set_objective(spob.EffectiveImmunogenicityObjective(upper_bound))
     problem.deactivate_constraint(spco.MinimumNTerminusCleavage)
     problem.deactivate_constraint(spco.MinimumCTerminusCleavage)
     problem.deactivate_constraint(spco.MaximumCleavageInsideEpitopes)
+    problem.add_constraint(spco.MinimumCoverageAverageConservation(protein_coverage, min_coverage=2, name='proteins'))
+    consconstr = spco.MinimumEffectiveConservation(0, name='proteins')
+    problem.add_constraint(consconstr)
+    problem.solve()
 
-    consconstr = None
+    #consconstr = None
     for i in range(1, rounds):
         cons = i * increment
-        if i == 1:
-            consconstr = spco.MinimumEffectiveConservation(increment, name='proteins')
+        if consconstr is None:
+            problem.add_constraint(
+                spco.MinimumCoverageAverageConservation(protein_coverage, min_coverage=2, name='proteins'),
+            )
+            consconstr = spco.MinimumEffectiveConservation(cons, name='proteins')
             problem.add_constraint(consconstr)
-        elif i > 1:
+        else:
             consconstr.update(min_conservation=cons)
 
         LOGGER.info('Solving with minimum effective conservation of %.3f', cons)
-        solution = problem.solve()
-        LOGGER.info('Effective immunogenicity: %.3f', aml.value(problem._model.EffectiveImmunogenicity))
+        solution = problem.solve(options={
+            'Threads': mp.cpu_count(),
+            'NonConvex': 2,
+            'MIPFocus': 1,  # if upper_bound is not None else 3,
+            # 'NodeMethod': 1,
+            'MIPGap': 0.01,
+        })
+        LOGGER.info('Effective immunogenicity: %.3f', aml.value(solution.immunogen))
 
         solution.to_csv(output_vaccine.format(cons))
 
